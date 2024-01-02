1ae4b0ad9e9ceab1229f05606a8def02436c771c
dejf <david.oravec@livesport.eu>
2024-01-02 17:10:22 +0100

    Add shipping icon and change shipping notice to full width

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from '@/src/components/Header';
 import { Analytics } from '@vercel/analytics/react';
 import HpTopImages from "@/src/components/HpTopImages";
 import IconColumns from "@/src/components/IconColumns";
@@ -7,6 +6,10 @@ import Carousel from "@/src/components/carousel/Carousel";
 import useFetchHpTopImages from "@/src/hooks/useFetchHpTopImages";
 import useFetchTopProducts from "@/src/hooks/useFetchTopProducts";
 import ImageAndDescription from "@/src/components/ImageAndDescription";
+import ShippingNotice from "@/src/components/ShippingNotice";
+import {useTranslation} from "@/src/hooks/useTranslation";
+import LanguageSwitch from "@/src/components/LanguageSwitch";
+import Navbar from "@/src/components/Navbar";
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -15,22 +18,27 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
     const { hpTopLeft, hpTopRight, error: errorImages } = useFetchHpTopImages();
     const { topProducts, error: errorProducts } = useFetchTopProducts();
+    const trans = useTranslation();
 
     if (errorImages || !hpTopLeft || !hpTopRight || errorProducts) {
         return <div>Error: {errorImages || errorProducts}</div>;
     }
 
     return (
-        <div className="columns is-gapless is-centered has-background-white">
-            <div className="column is-8 is-offset-2">
-                <Header/>
-                {hpTopLeft && hpTopRight && <HpTopImages leftImage={hpTopLeft} rightImage={hpTopRight}/>}
-                <IconColumns/>
-                {topProducts && <Carousel products={topProducts}/>}
-                <ImageAndDescription/>
+        <>
+            <ShippingNotice notice={trans('app.shipping_notice')}/>
+            <div className="columns is-gapless is-centered has-background-white">
+                <div className="column is-8 is-offset-2">
+                    <LanguageSwitch/>
+                    <Navbar/>
+                    {hpTopLeft && hpTopRight && <HpTopImages leftImage={hpTopLeft} rightImage={hpTopRight}/>}
+                    <IconColumns/>
+                    {topProducts && <Carousel products={topProducts}/>}
+                    <ImageAndDescription/>
+                </div>
+                <Analytics/>
             </div>
-            <Analytics/>
-        </div>
+        </>
     );
 };
 
--- src/components/ShippingNotice.tsx
+++ src/components/ShippingNotice.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 interface ShippingNoticeProps {
     notice: string;
@@ -7,6 +8,7 @@ interface ShippingNoticeProps {
 const ShippingNotice = ({ notice }: ShippingNoticeProps) => {
     return (
         <div className="shipping-notice has-text-white has-background-black has-text-centered is-size-7">
+            <Image src={"/shipping.svg"} alt={"Shipping icon"} width={13} height={13} className={"mr-2 mt-1"}/>
             {notice}
         </div>
     );
