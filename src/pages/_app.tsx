e26da01b977f0a6d7a67e1eefc647a9a35633aa2
dejf <david.oravec@livesport.eu>
2024-02-05 15:00:41 +0100

    Change favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,19 @@ function App({ Component, pageProps }: AppProps) {
                         name="description"
                         content="Eshop s prvotriednymi šperkami pre ženy"
                     />
+                    <link rel="icon" href="/favicon.ico" />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/favicon-16x16.png"
+                    />
                 </Head>
                 <Component {...pageProps} />
             </ArticleSlugsWithLocaleProvider>
