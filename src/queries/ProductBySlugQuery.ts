05bc4fb5dcf38b8b91c8d3f617669e1a61b33a72
dejf <david.oravec@livesport.eu>
2024-02-23 18:43:07 +0100

    Fix typo in GQL query

--- src/queries/ProductBySlugQuery.ts
+++ src/queries/ProductBySlugQuery.ts
@@ -24,7 +24,7 @@ export const ProductBySlugQuery = (slug: string, locale: SupportedLocale) => `{
       },
       lastPiecesText,
       newArrival,
-      bestseller,
+      bestSeller,
       slug,
     }
   }
