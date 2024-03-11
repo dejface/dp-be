ae8f39a00d9105f9a8719fe868875ce2bd117785
dejf <david.oravec@livesport.eu>
2024-03-11 15:06:16 +0100

    Add CartAddModal test

---
+++ src/components/product-detail/CartAddModal.test.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import CartAddModal from "@/src/components/product-detail/CartAddModal";
+import { userEvent } from "@testing-library/user-event";
+
+const setIsModalOpen = jest.fn();
+const title = "Test Product";
+
+jest.mock("@/contexts/TransContext", () => {
+    const { useTransMock } = require("../../../test/helpers/useTransMock");
+    return useTransMock({
+        "app.cart": "Kosik",
+        "app.cart.added": "Přidáno do košíku",
+    });
+});
+
+describe("CartAddModal", () => {
+    beforeEach(() => {
+        render(<CartAddModal title={title} setIsModalOpen={setIsModalOpen} />);
+    });
+
+    describe("setIsModalOpen correctly", () => {
+        test("correct call on modal background click", async () => {
+            await userEvent.click(screen.getByTestId("modal-background"));
+            expect(setIsModalOpen).toHaveBeenCalledWith(false);
+        });
+
+        test("correct call on close button click", async () => {
+            await userEvent.click(screen.getByTestId("cart-modal__close"));
+            expect(setIsModalOpen).toHaveBeenCalledWith(false);
+        });
+    });
+
+    describe("classnames", () => {
+        test("modal background", () => {
+            const modalBackground = screen.getByTestId("modal-background");
+
+            expect(modalBackground).toHaveClass("modal-background");
+            expect(modalBackground.parentElement).toHaveClass(
+                "modal is-active",
+            );
+        });
+
+        test("modal close button", () => {
+            const modalClose = screen.getByTestId("cart-modal__close");
+
+            expect(modalClose).toHaveClass("delete cart-modal__close");
+            expect(modalClose.parentElement).toHaveClass("box cart-modal__box");
+            expect(modalClose.parentElement?.parentElement).toHaveClass(
+                "modal-content has-text-centered",
+            );
+        });
+
+        test("heading", () => {
+            const heading = screen.getByText("Přidáno do košíku");
+
+            expect(heading).toHaveClass(
+                "is-size-4 has-text-weight-semibold mb-4",
+            );
+            expect(heading.parentElement).toHaveClass("box cart-modal__box");
+            expect(heading.parentElement?.parentElement).toHaveClass(
+                "modal-content has-text-centered",
+            );
+        });
+
+        test("product title", () => {
+            const productTitle = screen.getByText(title);
+
+            expect(productTitle).toHaveClass("is-size-5 mb-4");
+            expect(productTitle.parentElement).toHaveClass(
+                "box cart-modal__box",
+            );
+            expect(productTitle.parentElement?.parentElement).toHaveClass(
+                "modal-content has-text-centered",
+            );
+        });
+
+        test("link to cart", () => {
+            const linkToCart = screen.getByText("Kosik");
+
+            expect(linkToCart).toHaveAttribute("href", "/cart");
+            expect(linkToCart).toHaveClass("product__add-to-cart");
+            expect(linkToCart.parentElement).toHaveClass("box cart-modal__box");
+            expect(linkToCart.parentElement?.parentElement).toHaveClass(
+                "modal-content has-text-centered",
+            );
+        });
+    });
+});
