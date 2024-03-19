0a51aa606544f4b64a57c4705bc22b3a7c4f50de
dejf <david.oravec@livesport.eu>
2024-03-19 17:16:30 +0100

    Fix PopupModal test

--- src/components/PopupModal.test.tsx
+++ src/components/PopupModal.test.tsx
@@ -1,38 +1,48 @@
 import { render, act, screen } from "@testing-library/react";
-import VoucherDeniedModal from "@/src/components/cart/VoucherDeniedModal";
+import PopupModal from "@/src/components/PopupModal";
+import { CgDanger } from "react-icons/cg";
 
-jest.mock("@/contexts/TransContext", () => {
-    const { useTransMock } = require("../../../test/helpers/useTransMock");
-    return useTransMock({
-        "app.cart.voucher_denied": "Voucher denied",
-    });
-});
+describe("PopupModal", () => {
+    describe("functionality", () => {
+        it("closes modal after 2000ms", async () => {
+            const setIsModalOpen = jest.fn();
+            jest.useFakeTimers();
 
-describe("VoucherDeniedModal", () => {
-    it("closes modal after 2000ms", async () => {
-        const setIsModalOpen = jest.fn();
-        jest.useFakeTimers();
+            render(
+                <PopupModal
+                    setIsModalOpen={setIsModalOpen}
+                    text={"Voucher denied"}
+                    Icon={CgDanger}
+                    iconColor={"has-text-danger"}
+                />,
+            );
 
-        render(<VoucherDeniedModal setIsModalOpen={setIsModalOpen} />);
+            expect(setIsModalOpen).not.toHaveBeenCalled();
 
-        expect(setIsModalOpen).not.toHaveBeenCalled();
+            act(() => {
+                jest.advanceTimersByTime(2000);
+            });
 
-        act(() => {
-            jest.advanceTimersByTime(2000);
+            expect(setIsModalOpen).toHaveBeenCalledWith(false);
+
+            jest.useRealTimers();
         });
-
-        expect(setIsModalOpen).toHaveBeenCalledWith(false);
-
-        jest.useRealTimers();
     });
 
     describe("correct classnames", () => {
         test("icon", () => {
-            render(<VoucherDeniedModal setIsModalOpen={jest.fn()} />);
-            const icon = screen.getByTestId("voucher-denied-icon");
+            render(
+                <PopupModal
+                    setIsModalOpen={jest.fn()}
+                    Icon={CgDanger}
+                    iconColor={"has-text-danger"}
+                    text={"Voucher denied"}
+                />,
+            );
+            const icon = screen.getByTestId("popup-modal-icon");
             expect(icon).toHaveClass("is-size-1 has-text-danger mb-4");
             expect(icon.parentElement).toHaveClass(
-                "box cart-modal__box has-background-off-white",
+                "box cart-modal__box has-background-light-beige",
             );
             expect(icon.parentElement?.parentElement).toHaveClass(
                 "modal-content has-text-centered",
@@ -41,20 +51,27 @@ describe("VoucherDeniedModal", () => {
                 icon.parentElement?.parentElement?.parentElement,
             ).toHaveClass("modal is-active left-corner-modal");
         });
-    });
 
-    test("text", () => {
-        render(<VoucherDeniedModal setIsModalOpen={jest.fn()} />);
-        const text = screen.getByText("Voucher denied");
-        expect(text).toHaveClass("is-size-4 has-text-weight-semibold mb-4");
-        expect(text.parentElement).toHaveClass(
-            "box cart-modal__box has-background-off-white",
-        );
-        expect(text.parentElement?.parentElement).toHaveClass(
-            "modal-content has-text-centered",
-        );
-        expect(text.parentElement?.parentElement?.parentElement).toHaveClass(
-            "modal is-active left-corner-modal",
-        );
+        test("text", () => {
+            render(
+                <PopupModal
+                    setIsModalOpen={jest.fn()}
+                    Icon={CgDanger}
+                    iconColor={"has-text-danger"}
+                    text={"Voucher denied"}
+                />,
+            );
+            const text = screen.getByText("Voucher denied");
+            expect(text).toHaveClass("is-size-4 has-text-weight-semibold mb-4");
+            expect(text.parentElement).toHaveClass(
+                "box cart-modal__box has-background-light-beige",
+            );
+            expect(text.parentElement?.parentElement).toHaveClass(
+                "modal-content has-text-centered",
+            );
+            expect(
+                text.parentElement?.parentElement?.parentElement,
+            ).toHaveClass("modal is-active left-corner-modal");
+        });
     });
 });
