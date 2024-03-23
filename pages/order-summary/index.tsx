87072c586f302aa40a3d1d3a6caca0ed79d4fa10
dejf <david.oravec@livesport.eu>
2024-03-23 12:07:43 +0100

    Remove cart items and voucher code on first render

--- pages/order-summary/index.tsx
+++ pages/order-summary/index.tsx
@@ -2,16 +2,20 @@ import { GetServerSideProps } from "next";
 import Layout from "@/src/components/Layout";
 import StripeSingleton from "@/src/utils/getStripeInstance";
 import { GoCheckCircle } from "react-icons/go";
-import React from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "@/src/contexts/TransContext";
 
 interface OrderSummaryProps {
-    customerName: string;
     email: string;
 }
 
-const OrderSummaryIndex = ({ customerName, email }: OrderSummaryProps) => {
+const OrderSummaryIndex = ({ email }: OrderSummaryProps) => {
     const trans = useTranslation();
+
+    useEffect(() => {
+        localStorage.removeItem("shoppingCart");
+        localStorage.removeItem("voucherCode");
+    }, []);
 
     return (
         <Layout>
@@ -44,19 +48,19 @@ const OrderSummaryIndex = ({ customerName, email }: OrderSummaryProps) => {
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const sessionId = context.query.session_id as string;
     const stripe = StripeSingleton.getInstance();
+    const locale = context.locale;
     try {
         const session = await stripe.checkout.sessions.retrieve(sessionId);
 
         return {
             props: {
-                customerName: session.customer_details!.name as string,
                 email: session.customer_details!.email as string,
             },
         };
     } catch (err) {
         return {
             redirect: {
-                destination: "/",
+                destination: `/${locale ?? ""}`,
                 permanent: false,
             },
         };
