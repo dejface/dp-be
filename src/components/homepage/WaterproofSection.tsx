71a7eacd88f9eb9cdbb99e2573c96170b90c05b4
dejf <david.oravec@livesport.eu>
2024-03-23 19:51:57 +0100

    Edit waterproof section

--- src/components/homepage/WaterproofSection.tsx
+++ src/components/homepage/WaterproofSection.tsx
@@ -8,15 +8,15 @@ const WaterproofSection = () => {
     return (
         <section className="section is-paddingless pl-3 pr-3 pt-6">
             <div className="columns is-vcentered">
-                <div className="column is-full-mobile is-paddingless mr-6 pb-3-mobile px-1-mobile">
-                    <h3 className="title is-6 pb-3">
+                <div className="column is-full-mobile is-paddingless pb-3-mobile px-1-mobile">
+                    <h3 className="waterproof__title pb-3">
                         {trans("app.waterproof_section.title")}
                     </h3>
-                    <p className="subtitle is-6">
+                    <p className="waterproof__subtitle">
                         {trans("app.waterproof_section.description")}
                     </p>
                 </div>
-                <div className="column is-two-thirds is-paddingless is-full-mobile pl-3-desktop">
+                <div className="column is-two-thirds is-paddingless is-full-mobile pl-3-desktop px-1-tablet">
                     <figure className="image">
                         <Image
                             className={"waterproof__image"}
