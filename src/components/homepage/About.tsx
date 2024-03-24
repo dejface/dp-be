5f5e4d8f8400d493bc80b03cc76fa9b4538c32a1
dejf <david.oravec@livesport.eu>
2024-03-24 10:52:52 +0100

    Edits in about section

--- src/components/homepage/About.tsx
+++ src/components/homepage/About.tsx
@@ -6,34 +6,37 @@ const About = () => {
     const trans = useTranslation();
     return (
         <div className="section is-paddingless pt-6 pb-6 px-1-mobile">
-            <div className="columns is-variable is-8-mobile is-desktop">
+            <div className="columns">
                 <div className="column about__text-column">
-                    <h2 className="title">{trans("app.about.title")}</h2>
-                    <p className={"is-italic"}>
-                        {trans("app.about.first_paragraph")}
-                    </p>
-                    <br />
-                    <p className={"is-italic"}>
-                        {trans("app.about.second_paragraph")}
-                    </p>
-                    <br />
-                    <p className={"is-italic"}>
-                        {trans("app.about.third_paragraph")}
-                    </p>
-                    <br />
-                    <p className={"is-italic"}>
-                        {trans("app.about.fourth_paragraph")}
-                    </p>
+                    <h2 className="about__title mb-4">
+                        {trans("app.about.title")}
+                    </h2>
+                    <div className="about__paragraphs">
+                        <p className={"is-italic"}>
+                            {trans("app.about.first_paragraph")}
+                        </p>
+                        <br />
+                        <p className={"is-italic"}>
+                            {trans("app.about.second_paragraph")}
+                        </p>
+                        <br />
+                        <p className={"is-italic"}>
+                            {trans("app.about.third_paragraph")}
+                        </p>
+                        <br />
+                        <p className={"is-italic"}>
+                            {trans("app.about.fourth_paragraph")}
+                        </p>
+                    </div>
                 </div>
                 <div className="column about__image-column">
-                    <figure className="image is-paddingless">
-                        <Image
-                            src={"/placeholder.png"}
-                            alt={"alt"}
-                            width={474}
-                            height={483}
-                        />
-                    </figure>
+                    <Image
+                        className={"about__image"}
+                        src={"/placeholder.png"}
+                        alt={"alt"}
+                        width={474}
+                        height={483}
+                    />
                 </div>
             </div>
         </div>
