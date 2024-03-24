41d54d8b25749ebeb986c39d7ab1365096a810e7
dejf <david.oravec@livesport.eu>
2024-03-24 13:30:47 +0100

    Edits in Review section

--- src/components/homepage/review/Review.tsx
+++ src/components/homepage/review/Review.tsx
@@ -14,28 +14,24 @@ interface ReviewProps {
 
 const Review = (props: ReviewProps) => {
     return (
-        <div className="column is-one-third-desktop is-full-mobile">
+        <div className="column is-one-third-desktop is-one-third-tablet is-full-mobile">
             <div className="box review is-shadowless">
                 <div className="review__content has-text-centered">
                     <FaQuoteLeft
                         className="fa-icon mb-2"
                         data-testid={"quote-icon"}
                     />
-                    <p className="review__content review__description px-4 pb-3 pt-3">
+                    <p className="review__content review__content__description px-4 pb-3 pt-3">
                         {props.description}
                     </p>
-                    <figure className="image review__image is-64x64 mx-auto mt-4 mb-5">
-                        <Image
-                            src={props.image.url}
-                            alt={"Image of " + props.reviewer}
-                            width={64}
-                            height={64}
-                            className="is-rounded"
-                        />
-                        <figcaption className="is-size-7 mt-2">
-                            {props.reviewer}
-                        </figcaption>
-                    </figure>
+                    <Image
+                        src={props.image.url}
+                        alt={"Image of " + props.reviewer}
+                        width={64}
+                        height={64}
+                        className="is-rounded review__image mx-auto mt-3"
+                    />
+                    <div className="is-size-7 mt-2">{props.reviewer}</div>
                 </div>
             </div>
         </div>
