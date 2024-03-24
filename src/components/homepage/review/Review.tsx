1137869599de21796529ef8a5f9cf3be6bf5eea8
dejf <david.oravec@livesport.eu>
2024-03-24 09:37:44 +0100

    Edit Review section

--- src/components/homepage/review/Review.tsx
+++ src/components/homepage/review/Review.tsx
@@ -16,15 +16,15 @@ const Review = (props: ReviewProps) => {
     return (
         <div className="column is-one-third-desktop is-full-mobile">
             <div className="box review is-shadowless">
-                <div className="has-text-centered">
+                <div className="review__content has-text-centered">
                     <FaQuoteLeft
-                        className="fa-icon"
+                        className="fa-icon mb-2"
                         data-testid={"quote-icon"}
                     />
-                    <p className="review__description px-4 pb-3 pt-3">
+                    <p className="review__content review__description px-4 pb-3 pt-3">
                         {props.description}
                     </p>
-                    <figure className="image is-64x64 mx-auto mt-4 mb-5">
+                    <figure className="image review__image is-64x64 mx-auto mt-4 mb-5">
                         <Image
                             src={props.image.url}
                             alt={"Image of " + props.reviewer}
